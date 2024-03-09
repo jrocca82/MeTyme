506543f6723a0da08f322f39ec78b9ec06bbe8f7
Jo <loluvulol@gmail.com>
2024-03-10 09:49:36 +1000

    feat: seo setup

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -3,10 +3,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@metyme/ui";
 import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
+import { NextSeo } from 'next-seo';
 
 const App = ({ Component, pageProps }: AppProps) => (
       <ChakraProvider theme={theme}>
           <Analytics />
+          <NextSeo
+            title="Home Page Title"
+            description="Home page description of the page"
+        />
           {/* @ts-ignore */}
           <Component {...pageProps} />
       </ChakraProvider>
