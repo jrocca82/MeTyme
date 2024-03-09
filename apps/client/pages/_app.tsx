c623b1755e172da6c516d5975df82af6707d5ea7
Jo <loluvulol@gmail.com>
2024-03-10 09:35:24 +1000

    fix:build

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,26 +1,15 @@
 import "@total-typescript/ts-reset";
-import '@splidejs/react-splide/css/core';
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@metyme/ui";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
-import { useState } from "react";
-import { useRouter } from "next/router";
 
-const App = ({ Component, pageProps }: AppProps) => {
-  const [queryClient] = useState(() => new QueryClient());
-  const router = useRouter();
-
-  return (
-    <QueryClientProvider client={queryClient}>
+const App = ({ Component, pageProps }: AppProps) => (
       <ChakraProvider theme={theme}>
           <Analytics />
           {/* @ts-ignore */}
           <Component {...pageProps} />
       </ChakraProvider>
-    </QueryClientProvider>
   );
-};
 
 export default App;
