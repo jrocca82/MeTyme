73b3a33ab111f53175ecc82487a8d2d97994276b
Jo Rocca <loluvulol@gmail.com>
2024-03-10 01:34:08 +0000

    fix: fonts

--- packages/ui/theme/fonts/index.tsx
+++ packages/ui/theme/fonts/index.tsx
@@ -5,6 +5,8 @@ export const montserrat = Montserrat({
 	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
+// Woo hoo! Since we didn't actually make any changes, we don't need to deploy. But if we did have changes, we can do:
+// Control+C to exit
 export const garamond = EB_Garamond({
 	subsets: ["latin", "latin-ext"],
 	weight: ["400", "500", "700", "800"],
