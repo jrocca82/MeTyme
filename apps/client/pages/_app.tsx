e396ca9d71efc5ea5ec7f5cf661e4cc000eb3d8d
Jo <loluvulol@gmail.com>
2024-03-10 11:07:48 +1000

    feat: favicon

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -4,17 +4,22 @@ import { theme } from "@metyme/ui";
 import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
 import { NextSeo } from 'next-seo';
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => (
-      <ChakraProvider theme={theme}>
-          <Analytics />
-          <NextSeo
-            title="Me Tyme"
-            description="Wellness services"
-        />
-          {/* @ts-ignore */}
-          <Component {...pageProps} />
-      </ChakraProvider>
-  );
+  <ChakraProvider theme={theme}>
+    <Head>
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
+    <Analytics />
+    <NextSeo
+      title="Me Tyme"
+      description="Wellness services"
+      
+    />
+    {/* @ts-ignore */}
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
 
 export default App;
